8aaa13c9b461c67be8c55141a976c566e763c4dd
sadhin28 <taosifsadhin113@gmail.com>
2025-04-15 23:13:30 +0600

    backtick practic

--- template.js
+++ template.js
@@ -6,6 +6,11 @@ console.log(name);
 const a =10;
 const b=20;
 // const result ='The sum of '+a+' and '+b+' is '+(a+b);
-
+//using back tick
 const result=`The sum of ${a} and ${b} is ${a+b}`
 console.log(result)
+
+//use backtick
+const email=`Taosif
+sadhin`
+console.log(email)
