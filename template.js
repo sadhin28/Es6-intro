441c863de509ca7c9c0d7a8c04531b64b36dbd24
sadhin28 <taosifsadhin113@gmail.com>
2025-04-15 23:04:45 +0600

    add a number

---
+++ template.js
@@ -0,0 +1,9 @@
+const first = 'jan'
+const last = 'pakhi'
+const greet='Potas Potas'
+const name = first +' '+ last+' '+greet;
+console.log(name);
+const a =10;
+const b=20;
+const result ='The sum of '+a+' and '+b+' is '+(a+b);
+console.log(result)
