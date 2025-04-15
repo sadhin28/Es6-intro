f118d6a7813d091965035dd46e8fa81d9642b851
sadhin28 <taosifsadhin113@gmail.com>
2025-04-15 23:07:33 +0600

    add backtick

--- template.js
+++ template.js
@@ -5,5 +5,7 @@ const name = first +' '+ last+' '+greet;
 console.log(name);
 const a =10;
 const b=20;
-const result ='The sum of '+a+' and '+b+' is '+(a+b);
+// const result ='The sum of '+a+' and '+b+' is '+(a+b);
+
+const result=`The sum of ${a} and ${b} is ${a+b}`
 console.log(result)
