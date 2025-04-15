23af63f835fc3e8080829b933a1f79f5e3128d63
sadhin28 <taosifsadhin113@gmail.com>
2025-04-16 00:24:13 +0600

    spread

---
+++ spread.js
@@ -0,0 +1,14 @@
+const max =Math.max(2,4,3,5,12,31,90)
+const numbers = [2,4,5,3,6,23,54,56,677,877];
+const arrayMax=Math.max(...numbers);
+console.log(...numbers)
+console.log(arrayMax)
+console.log(max)
+
+//use spread operator to compy
+const nums = [3,4,5,6];
+const nums2=nums;
+nums2.push(12);
+console.log(nums)
+nums.pop(12)
+console.log(nums)
