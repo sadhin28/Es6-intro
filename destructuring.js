f2945084a78944a46d15bd85da1a20b0501ac8d0
sadhin28 <taosifsadhin113@gmail.com>
2025-04-16 01:00:22 +0600

    object to destructuring method

---
+++ destructuring.js
@@ -0,0 +1,26 @@
+const actor = {
+    name:'Ananta',
+    age:'30',
+    phone:'0193828923',
+    money:523452
+}
+//use property name as a variable by Destructuring
+const {phone}=actor;
+const {age:boyos}=actor;
+
+// const phone=actor.Phone;
+console.log(phone)
+console.log(phone)
+console.log(phone)
+console.log(phone)
+// const name=actor.name;
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// console.log(name);
+// const age=actor.age;
+console.log(boyos)
+
+
+const a=[3,4,6,3,7];
+
