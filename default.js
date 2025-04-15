644c00a20aa255d6c9b2605bb0efe90081d5fa9b
sadhin28 <taosifsadhin113@gmail.com>
2025-04-15 22:49:24 +0600

    add two function

--- default.js
+++ default.js
@@ -1,7 +1,20 @@
-function add(num1, num2){
+function add(num1=0, num2=0){
     const result = num1+num2;
     console.log(num1,num2,result);
     return result;
 }
-const sum = add(6,9)
+// const sum = add(5,7)
+const sum=add()
 console.log(sum)
+function fullName(first,last){
+    const full=first + ' ' + last;
+    return full;
+}
+//if this function is hold array
+function friends(numbs=[]){
+
+}
+//if this function hold object
+function person(human={}){
+
+}
