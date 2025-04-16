2176ac2761094a8194b181140cf6299dc6583987
sadhin28 <taosifsadhin113@gmail.com>
2025-04-17 02:32:49 +0600

    map niya practic korlam

---
+++ doubled.js
@@ -0,0 +1,27 @@
+const numbers = [4,5,2,8,10];
+function doubleIt(num){
+    // console.log('num now ',num)
+    return num*2;
+}
+
+
+const result2 = numbers.map(n=>n*2);
+console.log(result2)
+
+// const double2 = x=>x*2;
+// const result =numbers.map(double2)
+// console.log(result)
+
+
+
+// const result = numbers.map(doubleIt)
+// console.log(result)
+
+// const doubled = [];
+// for(const num of numbers){
+//     const double=num*2;
+//     doubled.push(double)
+// }
+
+// console.log(doubled)
+
