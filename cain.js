88284743ee1cb05f43949a3ac9cf8e3b780f776c
sadhin28 <taosifsadhin113@gmail.com>
2025-04-17 02:03:35 +0600

    data access

---
+++ cain.js
@@ -0,0 +1,18 @@
+//data access
+const data=[
+    {id:1,name:'Taosif',address:'Barishal'},
+    {id:2,name:'Redwan',address:'Savar'},
+    {id:3,name:'Rayhan',address:'JamalPur'}
+]
+console.log(data[2].name)
+
+const products = {
+    count: 5000,
+    data:[
+        {id:1,name:'lenovo',price:75000},
+        {id:2,name:'mackbook',price:175000},
+        
+    ]
+}
+
+console.log(products.data[1].price)
