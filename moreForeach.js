bcc3bc331fe587f053b5436dfad7a7ae657d1f1b
sadhin28 <taosifsadhin113@gmail.com>
2025-04-17 19:28:50 +0600

    map foreach filter and find practic

---
+++ moreForeach.js
@@ -0,0 +1,14 @@
+const number=[4,56,23,78,23];
+const num = number.filter( n=>n> 50)
+console.log(num)
+
+
+
+const nums = number.find( n=>n> 50)
+console.log(nums)
+
+const numb=number.map(n=>n*2)
+console.log(numb)
+
+const numbs=number.forEach(n=>console.log(n*2))
+console.log(numbs)
