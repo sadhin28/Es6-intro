b3da0f701c8ccdd0cd4aadb6d11e5f1d1e7ced8b
sadhin28 <taosifsadhin113@gmail.com>
2025-04-18 18:53:16 +0600

    practic with argument

---
+++ common-js/passBy.js
@@ -0,0 +1,29 @@
+let num1=5;
+let num2=7;
+function multiply(n1,n2){
+    n1=27
+    const result = n1 * n2;
+    return result;
+}
+console.log(num1)
+const output = multiply(num1,num2)
+console.log(output)
+
+let student1 ={
+    name:'jalil',
+    partner:'Borsha'
+}
+let student2={
+     name:'Raj',
+    partner:'Anika'
+}
+
+function makeMovi(cupple1,cupple2){
+    cupple1.name = 'ononto'
+    cupple2.name='Sakib Khan'
+    console.log(student1,student2)
+}
+
+
+makeMovi(student1,student2)
+
