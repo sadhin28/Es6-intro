3d155a5433cdab596cf8058234fa67e0c23b05b8
sadhin28 <taosifsadhin113@gmail.com>
2025-04-25 02:16:35 +0600

    add product

--- localStore/card.js
+++ localStore/card.js
@@ -1,10 +1,20 @@
 const addproduct=()=>{
   const productName =  document.getElementById('product-name').value
-  
-  const quantity = document.getElementById('product-quantity').value
-  
-
-    localStorage.setItem(productName,quantity)
+  console.log(productName)
+  productName.value=''
+  const Quantity = document.getElementById('product-quantity').value
+  console.log(Quantity)
+  Quantity.value=''
+  displayProduct(productName,Quantity)
  
+}  
+
+const displayProduct = (p,q)=>{
+    const container = document.getElementById('productContainer')
+    const li= document.createElement('li')
+    li.innerText=`${p} : ${q}`;
+   
+    container.appendChild(li);
+
 
 }
