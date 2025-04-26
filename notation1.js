d7a56d119ef40fe87542fec72ae2372d46d28c7c
sadhin28 <taosifsadhin113@gmail.com>
2025-04-27 00:49:21 +0600

    practice bracket notation

--- notation1.js
+++ notation1.js
@@ -1,5 +1,5 @@
 const person ={
-    neme:'lal nil holud miya',
+    name:'lal nil holud miya',
     profession: 'Traffic Surgon',
     age:39,
     address:'shahabag'
@@ -10,3 +10,9 @@ const prof = person.profession;
 //bracket notation
 const prof2 = person['profession']
 console.log(prof2)
+
+//using bracket with variable string
+
+const pName = 'name';
+const prof3 = person[pName]
+console.log(prof3)
