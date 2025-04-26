712ffbb184a70381f5e6987ef0d3ef714a95e86e
sadhin28 <taosifsadhin113@gmail.com>
2025-04-26 23:25:13 +0600

    ternary

--- truthi-and-false.js
+++ truthi-and-false.js
@@ -12,3 +12,14 @@ if(!myMoney){
     //use check negative or falsy anythink
     
 }
+ 
+const money =80;
+let food;
+if(money>100){
+    food = 'briyani';
+}else{
+    food = 'cha biscuit'
+}
+let food1 = money>100? 'biriyani':'cha biscuit'
+console.log(food1)
+console.log(food)
