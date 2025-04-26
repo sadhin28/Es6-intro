479d8b93601f68e432d94fac06546703201e8468
sadhin28 <taosifsadhin113@gmail.com>
2025-04-26 23:12:32 +0600

    check truthy value

---
+++ truthi-and-false.js
@@ -0,0 +1,14 @@
+let myvar = 5;
+
+//vheck truthy
+if(myvar){
+    console.log('True')
+}else{
+    console.log("False")
+}
+
+let myMoney = 50;
+if(!myMoney){
+    //use check negative or falsy anythink
+    
+}
