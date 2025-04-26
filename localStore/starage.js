8c6d5005c31f065235301bbf46a35a342f8d8035
sadhin28 <taosifsadhin113@gmail.com>
2025-04-27 00:35:22 +0600

    add string in local storage

--- localStore/starage.js
+++ localStore/starage.js
@@ -15,3 +15,24 @@ const addToLocalStorage=()=>{
 const clearLocalStore = () =>{
     localStorage.clear()
 }
+
+const number = [10,30,50,80,90]
+const strNumber = JSON.stringify(number)
+localStorage.setItem("number Array",strNumber)
+ const numArray = localStorage.getItem('number Array')
+ console.log(typeof(numArray))
+ const strToObject = JSON.parse(numArray)
+console.log(strToObject)
+
+const person ={
+    name:'Sadhin',
+    age:23,
+    home:'Barishal'
+}
+
+const strObject = JSON.stringify(person);
+localStorage.setItem('person',strObject)
+
+const getPerson = localStorage.getItem('person')
+ const getPerstonStrtoObj = JSON.parse(getPerson)
+ console.log(getPerstonStrtoObj.name)
