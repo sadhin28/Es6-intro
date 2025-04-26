8b06632cd6f4dd3729a4f3d7b8ecccf8a9bf010b
sadhin28 <taosifsadhin113@gmail.com>
2025-04-26 23:42:56 +0600

    add operators

--- string-to-number.js
+++ string-to-number.js
@@ -1,4 +1,18 @@
+
 const input ='700'
 console.log(typeof(input));
 const strToNumber = +input;
 console.log(typeof(strToNumber))
+
+//
+
+const isActive = false;
+const showUser =()=>console.log('display User');
+const hideUser =()=>console.log('hide user');
+
+// isActive ? showUser() : hideUser();
+//use && if the left side is true then right side will be executed
+isActive && showUser() 
+//use || if the left side is false right side will be executed
+isActive || hideUser();
+
