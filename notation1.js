ab4f852e61704b00ecec532e7f7eacdcc9368a44
sadhin28 <taosifsadhin113@gmail.com>
2025-04-27 00:44:44 +0600

    dot notation

---
+++ notation1.js
@@ -0,0 +1,12 @@
+const person ={
+    neme:'lal nil holud miya',
+    profession: 'Traffic Surgon',
+    age:39,
+    address:'shahabag'
+}
+//dot notation
+const prof = person.profession;
+
+//bracket notation
+const prof2 = person['profession']
+console.log(prof2)
